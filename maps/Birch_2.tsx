5de81de76a8030a697f6e96ca63667398b92e24f
67Pao67 <paopaolufver@gmail.com>
2026-08-18 22:36:30 +0700

    game

---
+++ maps/Birch_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Birch_2" tilewidth="32" tileheight="32" tilecount="6" columns="2">
+ <image source="../GandalfHardcore FREE Platformer Assets/GandalfHardcore FREE Platformer Assets/Birch2.png" width="80" height="112"/>
+</tileset>
---
+++ maps/Other_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Other_tiles" tilewidth="32" tileheight="32" tilecount="63" columns="9">
+ <image source="../GandalfHardcore FREE Platformer Assets/GandalfHardcore FREE Platformer Assets/Other Tiles1.png" width="288" height="224"/>
+</tileset>
